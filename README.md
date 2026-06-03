948e7067e3d484584eb5de32fce6142fa25d76d9
dangdang602 <3037852325@qq.com>
2026-06-03 19:59:03 +0800

    v2:新增键盘输入功能

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # studyhomework
 Storage of assignments, lab reports and source code for one specified university course.
+v2更新：新增cin键盘输入交互功能，支持手动输入数字
--- ex01/Untitled1.cpp
+++ ex01/Untitled1.cpp
@@ -1,6 +1,10 @@
-#include<stdio.h> 
-
-int main() {
-    printf("Hello, World!\n");
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Hello World\n";
+    cout<<"请输入一个数字：";
+    cin>>num;        //【键盘输入核心代码】cin就是读取键盘输入
+    cout<<"你输入的数字："<<num<<endl;
     return 0;
 }
